b05e2e8f31781e44d607c1dd2088e1e5df16edea
Влад Морозов <vladmorozov2020@mail.ru>
2025-04-28 17:21:50 +0300

    tems

--- components/receiving_post.js
+++ components/receiving_post.js
@@ -1,6 +1,7 @@
 import { TelegramClient } from 'telegram';
 import { StringSession } from 'telegram/sessions/index.js';
 import input from 'input';
+import UserTheme from '../Them_model.js';
 import mongoose from 'mongoose';
 import axios from 'axios';
 import https from "https"
@@ -16,7 +17,7 @@ const agent = new https.Agent({
 
 const apiId = 21571955;
 const apiHash = 'e3e614651aba0bffc9b26526a3c83914';
-const SAVED_SESSION = '1AgAOMTQ5LjE1NC4xNjcuNTABu3jeIpODXBz/z6RYCZ8i2bjue2wWa5gHRUB7ZYEv+Crt+pMfEW7cIzgtbBW4kPblBHQThSEzzIXHzPqU2Mr8/iPfXs4WikM4m6ByI4Zss9ptSUjrAj8HVrcGUrtEh5dSJvW/oIqDripTfVWW4nPFgwZQmKR+pGqQRm4ais/gEXTzz8CPV4BXG7xXeGfUrX7vdWkgsGvU9LhjozQ18Y5zL/1eWQgvqbxtrG1nDGNs3ZCoquL29dB0wdPMq25boohPJpCC23w6I0Qz60gF1zGpqV2Gc90/vxcLoq0N8CXn1E3/WpKNqZxxD1e33gt84Zk4mG1JcnVY956eyVZ7QNLEtuk=';
+const SAVED_SESSION = '1AgAOMTQ5LjE1NC4xNjcuNTABuzktr3gRlutiXvrP+Uq2WnVC+egTVy6PWBvl3dx5ZLt8ciWqOrSMBlB7LC/2uKsneKyLhBFvjIA4QVXjNpA7s5ERI63p92KqnMNrFTfAcoNxYVp3NVFHx9elyO2j3/Jo/blHpic6ejjjBOfF6WulAAb8nty/OEubUILl8tNOMnCowe2+hWwMsPAfOz/YnlE4bzP/idrFLIsBA/OV72UFEEQVZeXdRMQplNBpvLawz8JnGDrTPzQP5vB13cTbc6EHDLAbGLud5dXyBA1Uzewq1k/WPW/UPgghssyAqeyIB0SBStUVqiHEB8W3MCgnix0GlbRSZaI+T9NPWQj5yvrdlNw=';
 const GIGACHAT_TOKEN = 'NWVlMWY2ZWUtZjgyMC00ODY2LWI1MDctOTI4NGE2M2FkZDE0OjU0MmUzM2ExLWI1YTItNDYwOS04MzdjLTU1ZjkxZGE3MTUyNQ=='
 
 const CHANNELS_TO_MONITOR = [
@@ -31,70 +32,26 @@ const CHANNELS_TO_MONITOR = [
 
 
 export const PostModels = {
-  klepinin: mongoose.model('klepininPost', new mongoose.Schema({
+  post_news: mongoose.model('newsPost', new mongoose.Schema({
     text: String,
     date: { type: Date, default: Date.now },
     channel: String,
     channelUsername: String,
     channelId: String,
     ssilkaPost: String,
+    tema: {  // Добавляем поле для темы
+      type: String,
+      required: true,
+      index: true  // Индекс для быстрого поиска
+    },
     tags: [String]
-  }), 'klepinin_posts'),
+  }), 'news_posts'),
 
-  it_cifrovizaciya: mongoose.model('it_cifrovizaciyaPost', new mongoose.Schema({
-    text: String,
-    date: { type: Date, default: Date.now },
-    channel: String,
-    channelUsername: String,
-    channelId: String,
-    ssilkaPost: String,
-    tags: [String]
-  }), 'it_cifrovizaciya_posts'),
-
-  mirera: mongoose.model('mireraPost', new mongoose.Schema({
-    text: String,
-    date: { type: Date, default: Date.now },
-    channel: String,
-    channelUsername: String,
-    channelId: String,
-    ssilkaPost: String,
-    tags: [String]
-  }), 'mirera_posts'),
-
-  roblox: mongoose.model('robloxPost', new mongoose.Schema({
-    text: String,
-    date: { type: Date, default: Date.now },
-    channel: String,
-    channelUsername: String,
-    channelId: String,
-    ssilkaPost: String,
-    tags: [String]
-  }), 'roblox_posts'),
-
-  ya_it_i: mongoose.model('ya_it_iPost', new mongoose.Schema({
-    text: String,
-    date: { type: Date, default: Date.now },
-    channel: String,
-    channelUsername: String,
-    channelId: String,
-    ssilkaPost: String,
-    tags: [String]
-  }), 'ya_it_iposts'),
-
-  other: mongoose.model('OtherPost', new mongoose.Schema({
-    text: String,
-    date: { type: Date, default: Date.now },
-    channel: String,
-    channelUsername: String,
-    channelId: String,
-    ssilkaPost: String,
-    tags: [String]
-  }), 'other_posts')
 };
 const GIGACHAT_AUTH_URL = 'https://ngw.devices.sberbank.ru:9443/api/v2/oauth';
 const GIGACHAT_API_URL = 'https://gigachat.devices.sberbank.ru/api/v1/chat/completions';
 const CLIENT_ID = '5ee1f6ee-f820-4866-b507-9284a63add14';
-const CLIENT_SECRET = '542e33a1-b5a2-4609-837c-55f91da71525';
+const CLIENT_SECRET = 'fb8ebde0-8ec4-44e3-81b7-f06f2d15ade0';
 
 
 
@@ -103,7 +60,7 @@ let gigaChatToken = null;
 let tokenExpiration = 0;
 
 async function getGigaChatToken() {
-
+  // Если токен еще действителен, возвращаем его
   if (gigaChatToken && Date.now() < tokenExpiration) {
     return gigaChatToken;
   }
@@ -134,14 +91,23 @@ async function getGigaChatToken() {
 }
 
 
-async function classifyPost(postText) {
+async function classifyPost(postText, tems, retries = 3) {
   try {
+
+    await new Promise(resolve => setTimeout(resolve, 1000))
+
+    let temms = tems
+    console.log(temms)
     const token = await getGigaChatToken();
     console.log(token)
     
-    const prompt = `Классифицируй пост (ответь только "klepinin", "it_cifrovizaciya", "ya_it_i", "mirera", "roblox" или "other")( "klepinin" - любое упоминание «Клепинин Павел; "it_cifrovizaciya" - "по теме: цифровизация/по теме: цифровая трансформация/по теме: ит/по теме: инновации/по теме: ии/по теме:меиавскленная в связках с ранхигс и президентская академия";"ya_it_i" - "любое упоминание «я-ИТ-ы»";"mirera" - "давай еще все про Mirera"; "roblox" - "всё про Roblox в связке с вуз и образование"; "other" - "всё не по таме"; ):
+    const prompt = `Проанализируй следующий новостной пост и определи, к какой теме из списка ${temms} он относится. Выбери только одну наиболее подходящую тему. Ответ должен быть точным совпадением с одним из элементов списка (включая регистр и формулировку). Если пост не соответствует ни одной теме, верни "другое".  
 
-  Пост: "${postText.substring(0, 500)}"`;
+   Текст поста:  
+   "${postText.substring(0, 500)}"
+
+    Ответ (только название темы, без пояснений, кавычек и лишних символов): `
+    
     const response = await axios.post(
       GIGACHAT_API_URL,
       {
@@ -160,33 +126,41 @@ async function classifyPost(postText) {
         timeout: 5000
       }
     );
-
     const classification = response.data.choices[0].message.content.trim().toLowerCase();
-    return ['klepinin', 'it_cifrovizaciya', 'ya_it_i', 'mirera', 'roblox'].includes(classification) ? classification : 'other';
+    console.log(classification, 'class')
+    return temms.includes(classification) ? classification : 'другое';
   } catch (error) {
+    if (error.response?.status === 429 && retries > 0) {
+      await new Promise(resolve => setTimeout(resolve, 3000));
+      return classifyPost(postText, tems, retries - 1);
+    }
     if (error.response?.status === 401 && retries > 0) {
-      return classifyPost(postText, retries - 1);
+      return classifyPost(postText, retries - 1);             
     }
     console.error('⚠️ Ошибка классификации:', error.response?.data || error.message);
-    return 'other';
+    return 'другое';
   }
 }
 
 
-async function savePost(postData) {
+async function savePost(postData, userThemes) {
   try {
-    const postType = await classifyPost(postData.text);
-    await new PostModels[postType](postData).save();
-    console.log(`💾 Сохранено в ${postType}_posts`);
+    const postType = await classifyPost(postData.text, userThemes);
+    console.log(postType, 'postType')
+    postData.tema = postType
+    await new PostModels["post_news"](postData).save();
+    console.log(`💾 Сохранено с темой ${postType}_posts`);
   } catch (error) {
     console.error('⚠️ Ошибка сохранения:', error);
   }
 }
 
-export async function main() {
+export async function main(telegramId) {
   try {
-    await mongoose.connect('mongodb+srv://vladmorozov2020:Nevskifront208@moroz.gjylj0v.mongodb.net/telegram?retryWrites=true&w=majority&appName=Moroz');
-    console.log('✅ Подключено к MongoDB');
+    console.log(telegramId)
+  await mongoose.connect('mongodb+srv://vladmorozov2020:Nevskifront208@moroz.gjylj0v.mongodb.net/teleg_news?retryWrites=true&w=majority&appName=Moroz')
+  .then(() => console.log('✅ MongoDB connected'))
+  .catch(err => console.error('❌ MongoDB error:', err));
 
     const client = new TelegramClient(new StringSession(SAVED_SESSION), apiId, apiHash, {
       connectionRetries: 5,
@@ -194,6 +168,13 @@ export async function main() {
 
     await client.connect();
     console.log('✅ Авторизован через сессию');
+
+    const user = await UserTheme.findOne({ telegramId: telegramId });
+    if (!user) {
+      console.log(`Пользователь ${telegramId} не найден, создаём нового`);
+      await UserTheme.create({ telegramId, themes: ['другое'] });
+    }
+
 
 
     const channelsInfo = {};
@@ -214,6 +195,8 @@ export async function main() {
 
     client.addEventHandler(async (event) => {
       try {
+        
+       
         if (!['UpdateNewChannelMessage', 'UpdateNewMessage'].includes(event.className)) return;
   
         const msg = event.message;
@@ -229,25 +212,31 @@ export async function main() {
   
         const channel = channelsInfo[sourceId];
         console.log(`📩 Пост из ${channel.title}`);
-  
+        
+        
+        const userThemes = await UserTheme.findOne({ telegramId: telegramId }); // Исправлено здесь
+        const themes = userThemes?.themes;
+
+        console.log("темы:", themes)
         await savePost({
           text: msg.message,
           channel: channel.title,
           channelUsername: channel.username,
           channelId: channel.id,
-          ssilkaPost: `https://t.me/${channel.username}/${msg.id}`
-        });
+          ssilkaPost: `https://t.me/${channel.username}/${msg.id}`,
+          tema: ""
+        }, themes);
       } catch (error) {
-        console.error(' Ошибка обработки:', error);
+        console.error('⚠️ Ошибка обработки:', error);
       }
     });
   
-    console.log(' Бот запущен и слушает сообщения...');
+    console.log('👂 Бот запущен и слушает сообщения...');
     await client.connect();
   
 
   } catch (err) {
-    console.error('Критическая ошибка:', err);
+    console.error('❌ Критическая ошибка:', err);
     process.exit(1);
   }
 }
--- index.js
+++ index.js
@@ -1,83 +1,149 @@
-import TelegramBot from 'node-telegram-bot-api'
-import mongoose from "mongoose"
-import { PostModels } from './components/receiving_post.js'
+import TelegramBot from 'node-telegram-bot-api';
+import mongoose from 'mongoose';
+import UserTheme from './Them_model.js';
+import { PostModels } from './components/receiving_post.js';
 import { main } from './components/receiving_post.js';
-const TOKEN = "7671893383:AAEG7rE4hov76OagJLuZhaf1o60i_119_ec"
 
+const TOKEN = "8118538983:AAE-g9pWvdC6qlOZj2h6ywS2OQAZt4S4OTo";
 const bot = new TelegramBot(TOKEN, {polling: true});
 
-mongoose.connect('mongodb+srv://vladmorozov2020:Nevskifront208@moroz.gjylj0v.mongodb.net/telegram?retryWrites=true&w=majority&appName=Moroz')
-.then(() => console.log('✅ MongoDB connected'))
-.catch(err => console.error('❌ MongoDB error:', err));
 
-await main();
+mongoose.connect('mongodb+srv://vladmorozov2020:Nevskifront208@moroz.gjylj0v.mongodb.net/teleg_news?retryWrites=true&w=majority&appName=Moroz')
+  .then(() => console.log('✅ MongoDB connected'))
+  .catch(err => console.error('❌ MongoDB error:', err));
 
-const themesKeyboard = {
-  reply_markup: {
-    keyboard: [
-      [{ text: 'Павел Клепинин' }, { text: 'IT' }],
-      [{ text: '«я-ИТ-ы»' }, { text: 'Mirera' }, { text: 'Roblox в связке с вуз и образование'}, {text: 'другое'}]
-    ],
-    resize_keyboard: true,
-    one_time_keyboard: true
+
+const userStates = new Map();
+
+async function getOrCreateUser(telegramId) {
+  try {
+    let user = await UserTheme.findOne({telegramId});
+    if (!user) {
+      user = new UserTheme({telegramId: telegramId});
+      await user.save();
+      console.log(`Создан новый пользователь с ID: ${telegramId}`);
+    }
+    return user;
+  } catch (err) {
+    console.error('Ошибка в getOrCreateUser:', err);
+    throw err;
   }
-};
+}
 
-bot.onText("/start", (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Выберите тему:', themesKeyboard);
+async function generateKeyboard(telegramId) {
+  const user = await getOrCreateUser(telegramId);
+  const buttons = [
+    ...user.themes.map(theme => [{text: `тема: ${theme}`}]),
+    [{text: 'Добавить тему'}, {text: 'Удалить тему'}]
+  ];
+  return { reply_markup: { keyboard: buttons, resize_keyboard: true } };
+}
+
+bot.onText(/\/start/, async (msg) => {
+  await main(msg.from.id);
+  const keyboard = await generateKeyboard(msg.from.id);
+  await bot.sendMessage(msg.chat.id, 'Выберите тему:', keyboard);
+});
+
+
+bot.onText(/^Добавить тему$/, async (msg) => {
+  userStates.set(msg.chat.id, { action: 'addingTheme' });
+  await bot.sendMessage(msg.chat.id, 'Введите название новой темы (она должна быть с маленькой буквы, можно несколько слов):');
 });
 
 
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
-  const text = msg.text;
+  const text = msg.text?.trim();
+  const userId = msg.from.id;
+
+  if (!text || text.startsWith('/')) return;
+
+  const userState = userStates.get(chatId);
+  const user = await getOrCreateUser(userId);
 
 
-  if (text.startsWith('/')) return;
+  if (userState?.action === 'addingTheme') {
+    userStates.delete(chatId);
 
-  try {
-    let posts = [];
-    
-    if (text === 'Павел Клепинин') {
-      posts = await PostModels.klepinin.find().sort({ date: 1 }).limit(5);
-    } else if (text === 'IT') {
-      posts = await PostModels.it_cifrovizaciya.find().sort({ date: 1 }).limit(5);
-    } else if (text === '«я-ИТ-ы»') {
-      posts = await PostModels.ya_it_i.find().sort({ date: 1 }).limit(5);
-    } else if (text === 'Mirera') {
-      posts = await PostModels.mirera.find().sort({ date: 1 }).limit(5);
-    } else if (text === 'Roblox в связке с вуз и образование') {
-      posts = await PostModels.roblox.find().sort({ date: 1 }).limit(5);
-    } else if (text === 'другое') {
-      posts = await PostModels.other.find().sort({ date: 1 }).limit(5);
-    }else {
-      return;
+
+    if (text.length < 2) {
+      return bot.sendMessage(chatId, 'Название темы должно содержать минимум 2 символа');
     }
 
-    if (posts.length === 0) {
-      await bot.sendMessage(chatId, `Пока нет сохранённых постов по теме "${text}".`);
-      return;
+
+    if (user.themes.includes(text)) {
+      return bot.sendMessage(chatId, 'Такая тема уже существует!');
     }
 
-    for (const post of posts) {
-      await bot.sendMessage(
-        chatId,
-        `<b>Канал:</b> ${post.channel}\n` +
-        `<b>Текст:</b> ${post.text}\n` +
-        `<b>Дата:</b> ${post.date.toLocaleString()}`,
-        {
-          parse_mode: 'HTML',
-          reply_markup: {
-            inline_keyboard: [
-              [{ text: "🔗 Открыть пост", url: post.ssilkaPost }]
-            ]
-          }
-        }
-      )
+    try {
+      await user.addTheme(text);
+      const keyboard = await generateKeyboard(userId);
+      await bot.sendMessage(chatId, `✅ Тема "${text}" успешно добавлена!`, keyboard);
+    } catch (err) {
+      console.error('Ошибка добавления темы:', err);
+      await bot.sendMessage(chatId, '❌ Произошла ошибка при добавлении темы');
     }
-  } catch (err) {
-    console.error('Ошибка:', err)
-    await bot.sendMessage(chatId, 'Произошла ошибка. Попробуйте позже.')
+    return;
   }
-})
+
+
+  if (user.themes.includes(text)) {
+    try {
+      const posts = await PostModels.post_news.find({tema: text}).sort({date: 1}).limit(5);
+      
+      if (!posts.length) {
+        return bot.sendMessage(chatId, `По теме "${text}" пока нет сохранённых постов.`);
+      }
+
+      for (const post of posts) {
+        await bot.sendMessage(
+          chatId,
+          `<b>Тема:</b> ${post.tema}\n` +
+          `<b>Канал:</b> ${post.channel}\n` +
+          `<b>Текст:</b> ${post.text}\n` +
+          `<b>Дата:</b> ${post.date.toLocaleString()}`,
+          {
+            parse_mode: 'HTML',
+            reply_markup: {
+              inline_keyboard: [[{text: "🔗 Открыть пост", url: post.ssilkaPost}]]
+            }
+          }
+        );
+      }
+    } catch (err) {
+      console.error('Ошибка поиска постов:', err);
+      await bot.sendMessage(chatId, '❌ Произошла ошибка при поиске постов');
+    }
+  }
+});
+
+bot.onText(/^Удалить тему$/, async (msg) => {
+  const user = await getOrCreateUser(msg.from.id);
+  
+  if (user.themes.length === 0) {
+    return bot.sendMessage(msg.chat.id, 'У вас нет тем для удаления');
+  }
+  
+  const keyboard = {
+    reply_markup: {
+      keyboard: user.themes.map(theme => [{text: `Удалить ${theme}`}]),
+      resize_keyboard: true
+    }
+  };
+  
+  await bot.sendMessage(msg.chat.id, 'Выберите тему для удаления:', keyboard);
+});
+
+bot.onText(/^Удалить (.+)$/, async (msg, match) => {
+  const themeName = match[1];
+  const user = await getOrCreateUser(msg.from.id);
+  
+  if (!user.themes.includes(themeName)) {
+    return bot.sendMessage(msg.chat.id, 'Такой темы нет');
+  }
+  
+  await user.removeTheme(themeName);
+  const keyboard = await generateKeyboard(msg.from.id);
+  await bot.sendMessage(msg.chat.id, `Тема "${themeName}" удалена!`, keyboard);
+});
